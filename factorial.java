e1ef6b162d66fbe4217d7f4da0d98ec5e4e57854
Sandeep939230 <sandeepgaddam502@gmail.com>
2024-08-30 10:59:37 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,10 @@
+class factorial{
+    public static void main(String args[]){
+        int n=5;
+        int i=1;
+        for(i=0;i<=n;i++){
+            System.out.println(n*i)
+        }
+    }
+}
+
